5794e17d8fb18c6fef54395e1f395f88888d7411
raymundorsansores <raymundo.rsansores@gmail.com>
2019-03-27 12:26:52 -0500

    asd

--- src/TrackerRESTService.java
+++ src/TrackerRESTService.java
@@ -223,7 +223,8 @@ public class TrackerRESTService {
 		Integer size = data.size() - 1;
 		try {
 			//ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-			InputStream is = TrackerRESTService.class.getResourceAsStream("Template.xlsm");
+			InputStream is = TrackerRESTService.class.getResourceAsStream("/Template.xlsm");
+			System.out.println(is);
 			//MyTest.class.getResourceAsStream("/test.csv");
 			workbook = new XSSFWorkbook(is);
 			//workbook = new XSSFWorkbook(OPCPackage.open("resources/Template.xlsm"));
